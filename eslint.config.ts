c53c3f1158c4f537554abf9ba4fe015950f3b529
Jaidlab <240663663+Jaidlab@users.noreply.github.com>
2026-08-14 23:27:02 +0100

    chore: Normalized ESLint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,3 @@
-import type {Linter} from 'eslint'
-
 import {makeEslintConfig} from 'eslint-config-jaid'
 
-const config: Array<Linter.Config> = makeEslintConfig()
-
-export default config
+export default makeEslintConfig()
